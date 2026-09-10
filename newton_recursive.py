ae95d092ace60f7f375aa9093f5ef278d955435b
Kaelen Bober <kbober1@luc.edu>
2026-09-09 20:08:34 -0500

    semi final; got recursion working, added infinite loop break -- have to review Leo's coding rules before turn in

--- newton_recursive.py
+++ newton_recursive.py
@@ -1,24 +1,38 @@
-def newton_sqrt(a):
-    """Calculate the square root of a number using Newton's method."""
+def newton_sqrt(a,x=None, loops=0):
+    """Calculate the square root of a number using Newton's method and Recursion."""
     # Guard statements to handle edge cases
     if a < 0:
-        raise ValueError("Cannot compute square root of a negative number.")
+        raise ValueError("There is no squareroot of negative numbers.")
     elif a == 0:
         return 0
+    # Stops the code from running infinitely
+    elif loops > 1000:
+        return x
+
+    # Only want to update x outside of recursion once
+    if x == None:
+        x = a/2
+    
 
     # Define the tolerance level for convergence
     epsilon = 0.0001 
-    # Base 
-    x = a / 2.0
+    # Base case, if the tolerance is less than epsilon, we have found the 
+    if abs(x*x-a) < epsilon:
+        return x
+    # Recursive call for function
+    return newton_sqrt(a, recalcuateX(a,x), loops+1)
+        
 
-    newton_calculation(a) 
-    if abs(x * x - a) > epsilon and loops < 1000:
+# Recalculate x 
+def recalcuateX(a,x):
+    x = (x + (a/x)) / 2.0
+    return x
+ 
 
-    
+
+newton_sqrt(37)
 
 
-#this will do the sqrt calculations on a single value for a meaning I can call the function over and over instead of using a while loop
-def newton_calculation(a, loops=0):
-    x = (x + (a/x)) / 2
-    loops += 1
-    return x and loops
+
+
+
